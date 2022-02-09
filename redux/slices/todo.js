010ec0508a7b13ebdfe073be9debf1bcc2452d20
RETSKEY <bashlovka96@mail.ru>
2022-02-09 21:45:14 +0300

    object replace from array and fixed a bug with id

--- redux/slices/todo.js
+++ redux/slices/todo.js
@@ -4,23 +4,18 @@ import {HYDRATE} from "next-redux-wrapper";
 export const todoSlice = createSlice({
     name: 'todo',
     initialState: {
-        todo: {
-            1: {id: 1, message: "python"},
-            2: {id: 2, message: "react"},
-            3: {id: 3, message: "javaScript"}
-        }
+        todo: [
+            {message: "python"},
+            {message: "react"},
+            {message: "javaScript"}
+        ]
     },
     reducers: {
         addTodo(state, action) {
-            const newId = Object.keys(state.todo).length + 1;
-            console.log(newId)
-            state.todo[newId] = {
-                id: newId,
-                message: action.payload
-            }
+            state.todo.push({ message: action.payload})
         },
         removeTodo(state, action) {
-            delete state.todo[action.payload]
+            state.todo.splice(action.payload, 1)
         },
     },
 
